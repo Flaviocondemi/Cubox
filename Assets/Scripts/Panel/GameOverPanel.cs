47bb6e5382a1dac2fd7290a7f60d4f474d79d8f7
Flavio <condemi68@gmail.com>
2020-04-17 12:52:42 +0200

    GameOver reset score
    I've chosen to reset the score when the player lose the bottle

--- Assets/Scripts/Panel/GameOverPanel.cs
+++ Assets/Scripts/Panel/GameOverPanel.cs
@@ -8,9 +8,11 @@ public class GameOverPanel : MonoBehaviour
     public GameObject soudTrack;
     public GameObject textCountDown;
     public GameObject pauseMenu;
+    public string level;
 
     private void OnEnable()
     {
+        Score.resetScore("Score" + level);
         TouchControlButton.resetAxisValue();
         SoundManager.playSound("GameOver");
         soudTrack.SetActive(false);
